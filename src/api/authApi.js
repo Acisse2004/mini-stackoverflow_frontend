6ad2886f1dc233720516694f84bfb3f03e2d81e6
Abdoul-Aziz-Ndour <abdoulazizndour2704@gmail.com>
2026-06-24 12:38:40 +0000

    fix: URL API en dur pour production

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,4 +1,4 @@
-const BASE = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
+const BASE = 'https://mini-stackoverflow-backend-8kcy.onrender.com/api'
 
 export const authApi = {
   login: async (email, password) => {
--- src/api/questionsApi.js
+++ src/api/questionsApi.js
@@ -1,4 +1,4 @@
-const BASE = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
+const BASE = 'https://mini-stackoverflow-backend-8kcy.onrender.com/api'
 
 const getHeaders = () => {
   const token = localStorage.getItem('token')
